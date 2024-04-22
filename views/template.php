1e58a069f63507dc8d541cf80c9b41309c5ad25f
JJOrtizChaverra <juanjochaverra02@gmail.com>
2024-04-22 11:22:38 -0500

    Cambiando el idioma de la aplicacion, estableciendo los metadatos necesarios, y cambiando el titulo, esto desde el template.php

--- views/template.php
+++ views/template.php
@@ -1,9 +1,16 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
+
+    <meta name="description" content="Aplicacion de reciclaje">
+    <meta name="keywords" content="recoapp">
+    <meta name="author" content="recoapp">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="robots" content="index, follow">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+
+    <title>RecoApp</title>
 </head>
 <body>
     
